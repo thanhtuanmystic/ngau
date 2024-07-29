12235af9da6c026a06dae7db3a87ad978cc0ead0
thanhtuanmystic <tuanvp2001@gmail.com>
2024-07-29 16:51:20 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 // search-box open close js code
 let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .fa-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", () => {
+  navbar.classList.toggle("showInput");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("fa-search", "fa-times");
+  } else {
+    searchBox.classList.replace("fa-times", "fa-search");
+  }
+});
 
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
